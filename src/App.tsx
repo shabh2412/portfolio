48c0bb187a5ea0a17e8b8a4302544276677a4e85
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-08-20 01:10:51 +0530

    removed default section component and used custom section component within the hero and about component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Box, Heading } from "@chakra-ui/layout";
+import { Box } from "@chakra-ui/layout";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import myImage from "./assets/Cartoon.png";
-import { Section } from "react-scroll-section";
 import About from "./components/About/About";
 import meWithMac from "./assets/Cartoon.png";
-import Wave from "./components/Designs/Wave";
-import Title from "./components/Hero/Title";
 
 function App() {
 	return (
@@ -18,22 +15,17 @@ function App() {
 			}}>
 			<Navbar />
 			<Box bg="primary" color="white">
-				<Section id="top" style={{ paddingTop: "50px" }}>
-					<Hero
-						intro="Hi, My Name is"
-						primaryTitle="Rishabh Panesar"
-						secondaryTitle="I am a full-stack dev"
-						image={myImage}
-						description="I build interesting things that live on the Internet! 🚀 \n I love to transform ideas into reality using code and developing web-apps using the latest front-end technologies and full fleged APIs.\nCurrently I'm learning Typescript and Chakra UI."
-						hasCallToAction={true}
-						callToActionMessage="Connect with me"
-					/>
-				</Section>
-				<Section id="about" style={{ paddingTop: "50px", textAlign: "center" }}>
-					<Wave />
-					<Title title="About Me" />
-					<About image={meWithMac} />
-				</Section>
+				<Hero
+					elementId="top"
+					intro="Hi, My Name is"
+					primaryTitle="Rishabh Panesar"
+					secondaryTitle="I am a full-stack dev"
+					image={myImage}
+					description="I build interesting things that live on the Internet! 🚀 \n I love to transform ideas into reality using code and developing web-apps using the latest front-end technologies and full fleged APIs.\nCurrently I'm learning Typescript and Chakra UI."
+					hasCallToAction={true}
+					callToActionMessage="Connect with me"
+				/>
+				<About image={meWithMac} />
 			</Box>
 		</div>
 	);
